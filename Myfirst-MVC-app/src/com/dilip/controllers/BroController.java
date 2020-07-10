f04e8c13d6c2d2e4d2207f7d315913201ad36e9a
DilipKumarGangwar <gangwardilipkumar@gmail.com>
2020-07-11 03:15:03 +0530

    first commit

---
+++ Myfirst-MVC-app/src/com/dilip/controllers/BroController.java
@@ -0,0 +1,17 @@
+package com.dilip.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BroController {
+	
+	
+	@ResponseBody
+	@RequestMapping("/bat")
+	public String giveCricketBat() {
+		return "ok..take the bat";
+	}
+
+}
